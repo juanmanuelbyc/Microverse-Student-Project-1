205a43f2a7459aa5f7a482fe9427b5c8d00b8653
Bateyjosue <josuebatey19@gmail.com>
2022-07-18 17:27:21 +0200

    hide menu when click on nav link

--- js/index.js
+++ js/index.js
@@ -1,15 +1,21 @@
 let btn = document.querySelector('#hamburguer-btn');
 let closeBtn = document.querySelector('.close-btn');
 let mobilenavlinks = document.querySelectorAll('.m-navlink');
+let al = document.querySelectorAll('*:not(#hamburguer-btn,.m-navlink)');
+
 
 btn.addEventListener('click', function(event) {
     document.getElementById('mobile-menu').style.visibility = 'visible';
+    
 });
 
 closeBtn.addEventListener('click', function(event) {
     document.getElementById('mobile-menu').style.visibility = 'hidden';
+
 });
 
-mobilenavlinks.addEventListener('click', function(event) {
-    document.getElementById('mobile-menu').style.visibility = 'hidden';
+mobilenavlinks.forEach((el)=>{
+    el.addEventListener('click', function(event) {
+        document.getElementById('mobile-menu').style.visibility = 'hidden';
+    });
 });
