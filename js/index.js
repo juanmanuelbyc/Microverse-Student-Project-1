076c3018bf4efb2b97384d82d32433f6f1c1de07
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-07-18 12:18:26 -0400

    correct some linters errors

--- js/index.js
+++ js/index.js
@@ -2,16 +2,16 @@ const btn = document.querySelector('#hamburguer-btn');
 const closeBtn = document.querySelector('.close-btn');
 const mobilenavlinks = document.querySelectorAll('.m-navlink');
 
-btn.addEventListener('click', function(event) {
-    document.getElementById('mobile-menu').style.visibility = 'visible';
+btn.addEventListener('click', function () {
+  document.getElementById('mobile-menu').style.visibility = 'visible';
 });
 
-closeBtn.addEventListener('click', function(event) {
-    document.getElementById('mobile-menu').style.visibility = 'hidden';
+closeBtn.addEventListener('click', function () {
+  document.getElementById('mobile-menu').style.visibility = 'hidden';
 });
 
 mobilenavlinks.forEach((el) => {
-    el.addEventListener('click', function(event) {
-        document.getElementById('mobile-menu').style.visibility = 'hidden';
+  el.addEventListener('click', function () {
+    document.getElementById('mobile-menu').style.visibility = 'hidden';
   });
 });
