120054ff0d0d98307fdb97d494bd210b85ba6685
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-07-19 05:00:18 -0400

    make elements look blurry behind mobile menu

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
 const btn = document.querySelector('#hamburguer-btn');
 const closeBtn = document.querySelector('.close-btn');
 const mobilenavlinks = document.querySelectorAll('.m-navlink');
+const sections = document.querySelectorAll('.section');
 
 btn.addEventListener('click', () => {
   document.getElementById('mobile-menu').style.visibility = 'visible';
+  sections.forEach((sect) => {
+    sect.style.filter = 'blur(4px)';
+  });
 });
 
 closeBtn.addEventListener('click', () => {
   document.getElementById('mobile-menu').style.visibility = 'hidden';
+  sections.forEach((sect) => {
+    sect.style.filter = 'none';
+  });
 });
 
 mobilenavlinks.forEach((el) => {
   el.addEventListener('click', () => {
     document.getElementById('mobile-menu').style.visibility = 'hidden';
+    sections.forEach((sect) => {
+      sect.style.filter = 'none';
+    });
   });
 });
