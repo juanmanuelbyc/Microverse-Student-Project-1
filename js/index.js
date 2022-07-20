a4666a842306fb88611f9a1209665ca118c984a7
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-07-20 06:22:15 -0400

    update pop ups

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const projects = {
   project1: {
     pname: 'Tonic',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
     client: 'CANOPY',
     role: 'Back End Dev',
     year: '2015',
@@ -9,11 +10,14 @@ const projects = {
     tech2: 'css',
     tech3: 'JavaScript',
     smallimagelink: 'url("../images/p1image.png")',
-    bigimagelink: 'url("../images/p1imagebig.jpg")'
+    bigimagelink: 'url("../images/p1imagebig.jpg")',
+    livelink: 'url("www.google.com")',
+    srclink: 'url("www.google.com")'
   },
   project2: {
     pname: 'Multi-post Stories',
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
     role: 'Full Stack Dev',
     year: '2015',
     client: 'Facebook',
@@ -21,11 +25,14 @@ const projects = {
     tech2: 'Ruby on rails',
     tech3: 'css',
     smallimagelink: 'url("../images/p2image.png")',
-    bigimagelink: 'url("../images/p1imagebig.jpg")'
+    bigimagelink: 'url("../images/p1imagebig.jpg")',
+    livelink: 'url("www.google.com")',
+    srclink: 'url("www.google.com")'
   },
   project3: {
     pname: 'Facebook 360',
     description: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
     role: 'Full Stack Dev',
     client: 'Facebook',
     year: '2015',
@@ -33,11 +40,14 @@ const projects = {
     tech2: 'Ruby on rails',
     tech3: 'css',
     smallimagelink: 'url("../images/p3image.png")',
-    bigimagelink: 'url("../images/p1imagebig.jpg")'
+    bigimagelink: 'url("../images/p1imagebig.jpg")',
+    livelink: 'url("www.google.com")',
+    srclink: 'url("www.google.com")'
   },
   project4: {
     pname: 'Uber Navigation',
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    longdescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
     client: 'Uber',
     role: 'Lead Developer',
     year: '2018',
@@ -45,11 +55,50 @@ const projects = {
     tech2: 'Ruby on rails',
     tech3: 'css',
     smallimagelink: 'url("../images/p4image.png")',
-    bigimagelink: 'url("../images/p1imagebig.jpg")'
+    bigimagelink: 'url("../images/p1imagebig.jpg")',
+    livelink: 'url("www.google.com")',
+    srclink: 'url("www.google.com")'
   }
 };
 
+
+for (i=0;i<4;i++) {
+  let numbertotext = (i+1).toString();
+  let div = document.createElement('div');
+  div.id = 'project'+numbertotext;
+  div.className = 'project';
+  div.innerHTML = '<div class="pimage"></div>\
+    <div class="projectdetails">\
+      <div class="p-title">\
+        <h2 class="titletext">xxxxx</h2>\
+      </div>\
+      <ul class="details-list">\
+        <li class="client">xxxx</li>\
+        <li class="spacedot"></li>\
+        <li class="role">xxxxx</li>\
+        <li class="spacedot"></li>\
+        <li class="year">xxxx</li>\
+      </ul>\
+      <div class="p-description">\
+        <p class="pdescriptiontext">xxxx</p>\
+      </div>\
+      <div class="languages">\
+        <ul class="languages-list">\
+          <li class="lang tech1">xxx</li>\
+          <li class="lang tech2">xxx</li>\
+          <li class="lang tech3">xxx</li>\
+        </ul>\
+      </div>\
+      <div class="project-access btn">See project</div>\
+    </div>';
+  
+    document.querySelector('.projects').appendChild(div);
+  };
+
+
+
 const btn = document.querySelector('#hamburguer-btn');
+const btnSeePrjct = document.querySelectorAll('.project-access');
 const closeBtn = document.querySelector('.close-btn');
 const mobilenavlinks = document.querySelectorAll('.m-navlink');
 const sections = document.querySelectorAll('.section');
@@ -77,3 +126,89 @@ mobilenavlinks.forEach((el) => {
   });
 });
 
+let projectz = document.querySelectorAll('.project');
+
+for (i=0;i<projectz.length;i++) {
+let project = projectz[i];
+project.querySelector('.titletext').innerText = projects['project'+((i+1).toString())]['pname'];
+project.querySelector('.pdescriptiontext').innerText = projects['project'+((i+1).toString())]['description'];
+project.querySelector('.client').innerText = projects['project'+((i+1).toString())]['client'];
+project.querySelector('.role').innerText = projects['project'+((i+1).toString())]['role'];
+project.querySelector('.year').innerText = projects['project'+((i+1).toString())]['year'];
+project.querySelector('.tech1').innerText = projects['project'+((i+1).toString())]['tech1'];
+project.querySelector('.tech2').innerText = projects['project'+((i+1).toString())]['tech2'];
+project.querySelector('.tech3').innerText = projects['project'+((i+1).toString())]['tech3'];
+project.querySelector('.pimage').style.backgroundImage = projects['project'+((i+1).toString())]['smallimagelink'];
+};
+
+function createPopUp (el){
+  let project = el.closest('.project');
+  let div = document.createElement('div');
+  div.className = 'popUp';
+  div.innerHTML = 
+  '<div class="cardcontainer">\
+     <div class="p-title">\
+       <h2 class="titletext">xxxxx</h2>\
+       <a class="card-close-btn" href = "">x</a>\
+     </div>\
+     <ul class="details-list">\
+       <li class="client">xxxx</li>\
+       <li class="spacedot"></li>\
+       <li class="role">xxxxx</li>\
+       <li class="spacedot"></li>\
+       <li class="year">xxxx</li>\
+     </ul>\
+     <div class="cimage"></div>\
+     <div class="detailsproject">\
+       <div class="p-description">\
+         <p class="pdescriptiontext">xxxx</p>\
+       </div>\
+       <div class="langsAndBtns">\
+         <div class="languages">\
+           <ul class="languages-list">\
+             <li class="lang tech1">xxx</li>\
+             <li class="lang tech2">xxx</li>\
+             <li class="lang tech3">xxx</li>\
+           </ul>\
+         </div>\
+         <div class="btns">\
+           <div class="live-btn btn">\
+             <h3>See Live</h3>\
+             <div class="live-icon"></div>\
+           </div>\
+           <div class="btn src-btn">\
+             <h3>See Source</h3>\
+             <div class="src-icon"></div>\
+           </div>\
+         </div>\
+       </div>\
+     </div>\
+   </div>';
+  div.style.position = 'fixed';
+  
+
+let myname = project.id;
+
+
+div.querySelector('.titletext').innerText = projects[myname]['pname'];
+div.querySelector('.pdescriptiontext').innerText = projects[myname]['longdescription'];
+div.querySelector('.client').innerText = projects[myname]['client'];
+div.querySelector('.role').innerText = projects[myname]['role'];
+div.querySelector('.year').innerText = projects[myname]['year'];
+div.querySelector('.tech1').innerText = projects[myname]['tech1'];
+div.querySelector('.tech2').innerText = projects[myname]['tech2'];
+div.querySelector('.tech3').innerText = projects[myname]['tech3'];
+div.querySelector('.cimage').style.backgroundImage = projects[myname]['smallimagelink'];
+div.querySelector('.live-icon').style.backgroundImage = 'url("../images/liveicon.png")';
+div.querySelector('.src-icon').style.backgroundImage = 'url("../images/catvector.png")';
+document.body.prepend(div);
+
+
+
+}
+
+btnSeePrjct.forEach((el) => {
+  el.addEventListener('click', () => {
+    createPopUp (el);
+    });
+  });
